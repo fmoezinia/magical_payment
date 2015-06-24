df3c81360d6add7b5f47334564c9f8e9d1daefee
fmoezinia <moezinia.r@gmail.com>
2015-06-24 14:39:22 -0700

    init file

--- flask_payment/app/app_stripe.py
+++ flask_payment/app/app_stripe.py
@@ -1,3 +1,22 @@
+#database functionality. Collection of info comprising: phone number(user), shipping address, credit card token from stripe
+
+# from pymongo import MongoClient
+# client = MongoClient('mongodb://dbmagic:znmagic2@ds036698.mongolab.com:36698/darksidemagic')
+
+# REMEMBER TO DELETE PRACTICE DOCUMENTS ON MONGOLAB -- http://api.mongodb.org/python/current/tutorial.html
+
+# db = client.darksidemagic
+
+# data = {'user': '+17186121018', 
+# 'shipping': ['12 Upper Phillimore Gardens', 'w8 7ha', 'London', 'UK'],
+# 'stripe_INFO: 'xxxxx'
+# }
+
+# #user information is document containing data.
+# result = db.user_information.insert_one(data)   (LATER, DATA.APPEND)
+
+# db.user_information.find_one({'user': 'their number'}) returns dictionary.
+
 import os
 from flask import Flask, render_template, request
 import stripe
@@ -10,8 +29,10 @@ SECRET_KEY= 'sk_test_azDI1jLdKJ9QEi7HeS78xcS8'
 #   'publishable_key': os.environ['PUBLISHABLE_KEY']
 # }
 #SHOULD USE OS.ENVIRON TO HIDE PUBLIC KEYS. FOR TESTING ITS OK NOT TO
+#also have to get my own keys from stripe dashboard
 
- 
+data = {}
+
 stripe_keys = {
   'secret_key': SECRET_KEY,
   'publishable_key': PUBLISHABLE_KEY
@@ -23,25 +44,44 @@ app = Flask(__name__)
  
 @app.route('/')
 def index():
-  return render_template('index_stripe.html', key=stripe_keys['publishable_key'])
+  
+
+	return render_template('index_stripe.html', key=stripe_keys['publishable_key'])
  
 @app.route('/charge', methods=['POST'])
 def charge():
-	amount = 500
- 
+	
+ 	token = request.form['stripeToken']
  	customer = stripe.Customer.create(
-        email='customer@example.com',
-        card=request.form['stripeToken']
+        source = token,
+        description = '{{customer}} #their number',
+        metadata = {'address': 'etc'}	#'shipping info'
     )
- 
- 	charge = stripe.Charge.create(
-        customer=customer.id,
-        amount=amount,
-        currency='usd',
-        description='Flask Charge'
-    )
- 
- 	return render_template('charge_stripe.html', amount=amount)
+	
+
+	# Save the customer ID in your database so you can use it later
+	#save_stripe_customer_id(user, customer.id)
+	# Later...
+	#customer_id = get_stripe_customer_id(user)
+
+	stripe.Customer.retrieve(customer.id)
+	try:
+		stripe.Charge.create(
+		customer=customer.id, #or customer_id??,
+		amount=200*100,
+		#source = token,
+		currency='usd',
+		description='Amazin Charge'
+	)
+	except stripe.error.CardError, e:
+		pass
+ 	print customer.metadata
+ 	return render_template('charge_stripe.html')
  
 if __name__ == '__main__':
   app.run(debug=True)
+
+
+
+
+
--- flask_payment/app/db_info.py
+++ flask_payment/app/db_info.py
@@ -1,32 +1,35 @@
 #database functionality. Collection of info comprising: phone number(user), shipping address, credit card token from stripe
 
-
-
 from pymongo import MongoClient
 client = MongoClient('mongodb://dbmagic:znmagic2@ds036698.mongolab.com:36698/darksidemagic')
 
+HAVE TO DELETE PRACTICE DOCUMENTS ON MONGOLAB
+http://api.mongodb.org/python/current/tutorial.html
 
-data = [{'user': '+17186121018', 
+db = client.darksidemagic
+
+#data must be dictionary like json object
+data = {'user': '+17186121018', 
 'shipping': ['12 Upper Phillimore Gardens', 'w8 7ha', 'London', 'UK'],
 'stripe_token': 'xxxxx'
-}]
+}
 
-
+#user information is document containing data.
 result = db.user_information.insert_one(data)
 
 
 #The operation returns an InsertOneResult object, which includes an attribute inserted_id that contains the _id of the inserted document. Access the inserted_id attribute:
 
-result.inserted_id
+print result.inserted_id
 
-#>>> db.userinfo.find_one({'x': 1})
-#{u'x': 1, u'_id': ObjectId('54f112defba522406c9cc208')}
+db.userinfo.find_one({'x': 1})
+{u'x': 1, u'_id': ObjectId('54f112defba522406c9cc208')}
 
-#In MongoDB, the db.collection.insert() method adds new documents into a collection.
+In MongoDB, the db.collection.insert() method adds new documents into a collection.
 
-#db.user_information.find()
-#will return the data plus a _id section with objectID
+db.user_information.find()
+will return the data plus a _id section with objectID
 
-#db.user_information.replace_one()
-#if user shipping needs to be updated (use user phone number)
+db.user_information.replace_one()
+if user shipping needs to be updated (use user phone number)
 
